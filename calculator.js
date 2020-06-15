f6458d6277b123641803164a6e4a5b6d81a4b285
Luis E.Velasquez <luiseduardo@Luiss-MacBook-Pro.local>
2020-06-15 20:18:49 +0300

    calculator task day 2

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 // Define the numbers and the operation here
-
+const numOne = 2;
+const numTwo = 4;
+const totalSum = numOne + numTwo;
 // Write if-statements here
+If (totalSum > numTwo){
+    console.log("The operation used +")
+} else {
+    console.log("The operation did not use +")
+};
