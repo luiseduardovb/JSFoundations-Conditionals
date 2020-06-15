51d629b9f05fbda2881adec01430d92ccd136a40
Luis E.Velasquez <luiseduardo@Luiss-MacBook-Pro.local>
2020-06-15 20:24:06 +0300

    If statement task

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 // Define the numbers and the operation here
-const numOne = 2;
-const numTwo = 4;
-const totalSum = numOne + numTwo;
+const x = 2;
+const y = 4;
+const operation ; 
 // Write if-statements here
-If (totalSum > numTwo){
-    console.log("The operation used +")
-} else {
-    console.log("The operation did not use +")
-};
+
+
+
+if (operation === "+") {
+    console.log(x + y)
+}else if (operation === "-"){
+    console.log(x - y)
+}else if (operation === "/") {
+    console.log(x / y)
+}else {console.log(x * y)}
+
+
+
+
+
